ae39c205d799492ef518ded4b0cb457d8874199a
John Klamut <jmk322@pitt.edu>
2023-10-23 10:35:00 -0400

    Air Sensor

--- Bop-It/Bop-It.ino
+++ Bop-It/Bop-It.ino
@@ -1,7 +1,10 @@
+#define Pressure = 2
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   randomSeed(analogRead(0));
+  pinMode(Pressure, INPUT);
   
 
 }
@@ -14,6 +17,7 @@ int randomNumber(){
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  float value = analogRead(Pressure);
+  Serial.print(value);
 
 }
