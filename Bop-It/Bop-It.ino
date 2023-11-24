2ba32c314bd1a1cd9c8d119e1984179c10a09713
John Klamut <jmk322@pitt.edu>
2023-11-24 14:33:36 -0500

    Added Sound using ardunio

--- Bop-It/Bop-It.ino
+++ Bop-It/Bop-It.ino
@@ -8,36 +8,39 @@ bool input = true;
 
 const int background_tone[] = {400, 300, 250, 200, -1, 300, 350};
 const unsigned long background_duration[] = {200, 200, 150, 250, 100, 200, 250, 250};
+const int background_tone_size = 7;
 
 const int defeat_tone[] = {200, 175, 150, 125};
 const unsigned long defeat_duration[] = {300, 300, 300, 300};
+const int defeat_tone_size = 4;
 
 const int victory_tone[] = {300, 225, 350, 375, -1, 375, -1, 375};
 const unsigned long victory_duration[] = {150, 150, 150, 100, 50, 100, 50, 300};
+const int victory_tone_size = 8;
 
 const int success_tone[] = {300, 350, 400, 450};
 const unsigned long success_duration[] = {150, 150, 150, 150};
+const int success_tone_size = 4;
 
-const int reopen_tone[] = {500, -1, 500};
-const unsigned long reopen_duration[] = {100, 100, 100};
+const int cool_tone[] = {500, -1, 500};
+const unsigned long cool_tone_duration[] = {100, 100, 100};
+const int cool_tone_size = 3;
 
 const int button_tone[] = {-1, 200, -1};
 const unsigned long button_duration[] = {100, 100, 100};
+const int button_tone_size = 3;
 
 const int slide_tone[] = {150, 200, 250, 300, 350, 400};
 const unsigned long slide_duration[] = {50, 50, 50, 50, 50, 50};
+const int slide_tone_size = 6;
+
+const int flip_tone[] = {150, 200, 250, 300, 350, 400};
+const unsigned long flip_duration[] = {50, 50, 50, 50, 50, 50};
+const int flip_tone_size = 6;
 
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  lcd.init();
-  lcd.backlight();
 
-  pinMode(8, INPUT_PULLUP);
-  pinMode(6, OUTPUT);
-  
-}
+
 
 void success(){
   lcd.clear();
@@ -90,6 +93,28 @@ bool fanOn()
   return analogRead(A2)>60;
 }
 
+void play_tone(int sounds[], int duration[], int size){
+  //unsigned long last_tone_start = millis();
+  int current_tone = 0;
+
+  while (current_tone >= 0 && current_tone < size) {
+    tone(9, sounds[current_tone], duration[current_tone]);
+    delay(duration[current_tone]);
+    noTone(9);
+    current_tone++;
+  }
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  lcd.init();
+  lcd.backlight();
+
+  pinMode(8, INPUT_PULLUP);
+  pinMode(6, OUTPUT);
+  
+}
 
 void loop() {
   // put your main code here, to run repeatedly:
@@ -125,12 +150,15 @@ void loop() {
           lcd.print("Flip");
           condition = -1;
           input = true;
+          play_tone(flip_tone, flip_duration, flip_tone_size);
+          
         }
         else if(sliced(VPotStart)){
           lcd.clear();
           lcd.print("Sliced");
           condition = -1;
           input = true;
+          play_tone(slice_tone, slice_duration, slice_tone_size);
 
         }
         else if(fanOn()){
@@ -138,6 +166,7 @@ void loop() {
           lcd.print("Fan");
           condition = -1;
           input = true;
+          play_tone(cool_tone, cool_duration, cool_tone_size);
 
         }
         else if(pushedButton()){
@@ -145,14 +174,14 @@ void loop() {
           lcd.print("Pushed");
           condition = 1;
           input = true
+          play_tone(button_tone, button_duration, button_tone_size);
         }
         else{
           lcd.clear();
           lcd.print("No read");
         }
       }
-      //TODO: check correct sensor is pushed and not other sensors
-      
+      //TODO: check correct sensor is pushed and not other sensors     
   }
   else if(action == 2){
     lcd.clear();
@@ -164,12 +193,14 @@ void loop() {
           lcd.print("Flip");
           condition = -1;
           input = true;
+          play_tone(flip_tone, flip_duration, flip_tone_size);
         }
         else if(sliced(VPotStart)){
           lcd.clear();
           lcd.print("Sliced");
           condition = -1;
           input = true;
+          play_tone(slice_tone, slice_duration, slice_tone_size);
 
         }
         else if(fanOn()){
@@ -177,6 +208,7 @@ void loop() {
           lcd.print("Fan");
           condition = -1;
           input = true;
+          play_tone(cool_tone, cool_duration, cool_tone_size);
 
         }
         else if(pushedButton()){
@@ -184,6 +216,7 @@ void loop() {
           lcd.print("Pushed");
           condition = 1;
           input = true
+          play_tone(button_tone, button_duration, button_tone_size);
         }
         else{
           lcd.clear();
@@ -203,26 +236,28 @@ void loop() {
           lcd.print("Flip");
           condition = -1;
           input = true;
+          play_tone(flip_tone, flip_duration, flip_tone_size);
         }
         else if(sliced(VPotStart)){
           lcd.clear();
           lcd.print("Sliced");
           condition = -1;
           input = true;
-
+          play_tone(slice_tone, slice_duration, slice_tone_size);
         }
         else if(fanOn()){
           lcd.clear();
           lcd.print("Fan");
           condition = -1;
           input = true;
-
+          play_tone(cool_tone, cool_duration, cool_tone_size);
         }
         else if(pushedButton()){
           lcd.clear();
           lcd.print("Pushed");
           condition = 1;
           input = true
+          play_tone(button_tone, button_duration, button_tone_size);
         }
         else{
           lcd.clear();
@@ -241,26 +276,28 @@ void loop() {
           lcd.print("Flip");
           condition = -1;
           input = true;
+          play_tone(flip_tone, flip_duration, flip_tone_size);
         }
         else if(sliced(VPotStart)){
           lcd.clear();
           lcd.print("Sliced");
           condition = -1;
           input = true;
-
+          play_tone(slice_tone, slice_duration, slice_tone_size);
         }
         else if(fanOn()){
           lcd.clear();
           lcd.print("Fan");
           condition = -1;
           input = true;
-
+          play_tone(cool_tone, cool_duration, cool_tone_size);
         }
         else if(pushedButton()){
           lcd.clear();
           lcd.print("Pushed");
           condition = 1;
           input = true
+          play_tone(button_tone, button_duration, button_tone_size);
         }
         else{
           lcd.clear();
@@ -273,9 +310,12 @@ void loop() {
 
   if(condition == 1){
     success();
+    play_tone(success_tone, success_duration, success_tone_size);
   }
   else if(condition == -1){
+    play_tone(defeat_tone, defeat_duration, defeat_tone_size);
     incorrect();
+
   }
 
   if(game == false){
